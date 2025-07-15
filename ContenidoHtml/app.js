0372d11e7cdbd11ea158b82d6fab005a39c49dea
xMarianaC118x <marianacubillos13@gmail.com>
2025-07-15 12:26:29 -0500

    new

---
+++ ContenidoHtml/app.js
@@ -0,0 +1,12 @@
+'use strict';
+//función clásica de JS
+function evento(){
+    alert("Evento ejecutado");
+}
+document.addEventListener("DOMContentLoaded", function(){
+    //Seleccionar del dom el elemento que tiene el id modal
+    const myModal = document.getElementById('modal');
+    const btnOpen = document.getElementById('open');
+    const btnClose = document.getElementById('close');
+
+});
